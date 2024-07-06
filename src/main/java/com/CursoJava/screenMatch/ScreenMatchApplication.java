db6ba60b1e49234d86f42504c33b4c9a18bbf1bf
Oscar Acuña <oscarboris@outlook.cl>
2024-07-06 11:27:38 -0400

    carga inicial

---
+++ src/main/java/com/CursoJava/screenMatch/ScreenMatchApplication.java
@@ -0,0 +1,22 @@
+package com.CursoJava.screenMatch;
+
+import com.CursoJava.screenMatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		//var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=375c629f");
+		var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
