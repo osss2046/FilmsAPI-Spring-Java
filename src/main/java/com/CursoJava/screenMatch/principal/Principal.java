ac805593fbb1ecdd2e116d3e9ef4274e623472e5
Oscar Acuña <oscarboris@outlook.cl>
2024-07-07 17:46:10 -0400

    mapeo a base de datos 2

--- src/main/java/com/CursoJava/screenMatch/principal/Principal.java
+++ src/main/java/com/CursoJava/screenMatch/principal/Principal.java
@@ -2,11 +2,14 @@ package com.CursoJava.screenMatch.principal;
 
 import com.CursoJava.screenMatch.model.DatosSerie;
 import com.CursoJava.screenMatch.model.DatosTemporadas;
+import com.CursoJava.screenMatch.model.Serie;
 import com.CursoJava.screenMatch.service.ConsumoAPI;
 import com.CursoJava.screenMatch.service.ConvierteDatos;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
@@ -80,7 +83,13 @@ public class Principal {
     }
 
     private void mostrarSeriesBuscadas() {
-        datosSeries.forEach(System.out::println);
+        List<Serie> series = new ArrayList<>();
+        series = datosSeries.stream()
+                .map(d -> new Serie(d))
+                .collect(Collectors.toList());
+        series.stream().sorted(Comparator.comparing((Serie::getGenero)))
+                .forEach(System.out::println);
+
     }
 
 }
